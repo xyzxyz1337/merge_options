a1aadb37d44b3c55e21a1732e0bbecc5bc7e4cd1
zyxzyx1337 <gawi@1thecity.biz>
2019-03-26 03:25:37 -0400

    Init commit

---
+++ README.md
@@ -0,0 +1,50 @@
+#MERGE_OPTIONS
+
+This script merge two files with options in the format: option=value.
+
+##Usage
+
+
+		$ python3 merge_option.py prod.txt -c dev.txt 
+
+This command take options from prod.txt and print options which not in prod.txt. 
+This is do with *difference mode* by default. 
+
+The script have two mode: *difference* and *concatenate*. To understand a difference between him, take two files: prod.txt and dev.txt.
+
+```
+$ cat prod.txt
+
+java.mem=16g
+java.nonheapmem=1g
+clustermode=true
+```
+```
+$ cat dev.txt
+
+java.mem=20g
+clustermode=true
+cluster.name=java_cluster
+```
+###Difference
+
+```
+$ python3 merge_option.py prod.txt -c dev.txt --mode difference
+
+cluster.name=java_cluster
+```
+
+Why java.mem not in output? Because this filed exist in prod file. Difference not overwrite options.
+
+###Concatenate
+
+```
+$ python3 merge_option.py prod.txt -c dev.txt --mode concatenate
+
+cluster.name=java_cluster
+clustermode=true
+java.mem=20g
+java.nonheapmem=1g
+```
+
+So look this we lose a sequence, but all options was merged.
---
+++ merge_options.py
@@ -0,0 +1,128 @@
+import re
+import os
+import argparse
+
+
+def build_dict(file, delimiter, base_d=None, custom_d=None):
+    """
+    Open the file and parse every string.
+    Parsed data insert into one of dictionary,
+    required of base_flag
+    """
+    with open(file, "r") as rfile:
+        for string in rfile.readlines():
+            if string.startswith("#"):
+                continue
+            matched = re.findall(
+                r"(.+?)\s?{0}\s?(.*)".format(delimiter), string)
+            for i in matched:
+                i = list(i)  # because i is tuple, immutable
+                i[0] = i[0].strip()
+                i[1] = i[1].strip()
+
+                if custom_d is None:
+                    base_d[i[0]] = i[1]
+                else:
+                    custom_d[i[0]] = i[1]
+
+
+def difference(base_d, custom_d):
+    """
+    Print difference between base file and 
+    custom file. 
+    The equivalent fields will be passed.
+
+    Be meant the field in prod-file is unique.
+    """
+
+    option = "user configure"
+    print(head.format(option.upper()))
+
+    for key, value in custom_d.items():
+        if key not in base_d:
+            print(key, "=", value, sep="")
+
+        # elif value != base_d[key]:
+        #     print(key, "=", value, sep="")
+
+
+def concatenate(base_d, custom_d):
+    """
+    print sorted base file + custom file.
+    The equivalent key-option  value takes from custom file.
+    """
+    def where_is_dot(string):
+        """
+        return index of dot
+        or length (string)
+        """
+        dot = string.find(".")
+
+        if dot == -1:
+            dot = len(string)
+        return dot
+
+    base_d.update(custom_d)  # merge diction
+    kv_store_sorted = sorted(base_d.items())
+    option = kv_store_sorted[0][0]
+    option = option[:where_is_dot(option)]
+
+    print(head.format(option.upper()))
+
+    for key, value in kv_store_sorted:
+        dot = where_is_dot(key)
+
+        if option != key[:dot]:
+            option = key[:dot]
+            print(head.format(option).upper())
+
+        print(key, "=", value, sep="")
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="Script print difference or concatenate between base and custom files, but not overwrite!",
+        epilog="Attention! Mode \"concatenate\" sorts keys, you can lose structure")
+    parser.add_argument("base_files", metavar="BASE FILES", nargs="*",
+                        help="base files also know `prod-file`")
+
+    parser.add_argument("-c", metavar="CUSTOM FILES", nargs="*",
+                        help="custom files, which need merge", dest="custom")
+
+    parser.add_argument("--delimiter", metavar="SYMBOL", nargs="?", default="=",
+                        help="delimiter between option and value")
+
+    parser.add_argument("--mode", metavar="MODE", default="difference", choices=["difference", "concatenate"],
+                        help="exists two mode: difference and changed sum, by default: difference")
+    args = parser.parse_args()
+
+    if (args.base_files or args.custom) is None:
+        parser.print_help()
+        exit(1)
+
+    base = {}
+    custom_base = {}
+    head = """
+###########################################################################
+# {}
+###########################################################################
+"""
+
+    for b in args.base_files:
+        if not os.path.exists(b):
+            raise Exception('Path not found to file', b)
+        build_dict(b, base_d=base, delimiter=args.delimiter)
+
+    for c in args.custom:
+        if not os.path.exists(c):
+            raise Exception('Path not found to file', c)
+        build_dict(c, custom_d=custom_base, delimiter=args.delimiter)
+
+    assert((len(base) or len(custom_base)) >
+           0), "The program with this options not found elements.\nOptions: {}".format(args)
+
+    if args.mode == "difference":
+        difference(base, custom_base)
+    elif args.mode == "concatenate":
+        concatenate(base, custom_base)
